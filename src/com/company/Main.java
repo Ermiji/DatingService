d59b090fe6b4a0371ec5558856c64cf7ca776506
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-22 14:58:12 -0400

    Project completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,50 @@
 package com.company;
+import javax.swing.*;
 import java.util.ArrayList;
-import java.util.Random;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        Random rand = new Random();
-        ArrayList<Person> person = new ArrayList<>();
+        int random, rand;
+        ArrayList<Person> tempPerson1= new ArrayList<Person>();
+        ArrayList<Person> tempPerson2= new ArrayList<Person>();
+
+        ArrayList<Person> persons = new ArrayList<Person>();
 
         Person p = new Person("Ermias");
-        person.add(p);
-        p = new Person("Ariam");
-        person.add(p);
-        p = new Person("Sam");
-        person.add(p);
-        p = new Person("Daniel");
-        person.add(p);
-        p = new Person("Helen");
-        person.add(p);
-        p = new Person("Erica");
-        person.add(p);
+        persons.add(p);
+        Person p1 = new Person("Ariam");
+        persons.add(p1);
+        Person p2 = new Person("Sam");
+        persons.add(p2);
+        Person p3 = new Person("Weldu");
+        persons.add(p3);
+        Person p4 = new Person("Helen");
+        persons.add(p4);
+        Person p5 = new Person("Erica");
+        persons.add(p5);
 
-        System.out.println();
+        for(int i = 0; i< 10; i++) {
+
+            random = (int) (Math.random() * 5+1);
+            rand = (int)(Math.random()* 5+1);
+            if(random != rand){
+                tempPerson1.add(persons.get(random));
+                tempPerson2.add(persons.get(rand));
+
+            }
+
+
+        }
+        for(int i = 0; i< 10; i++) {
+
+            System.out.println(i + " " + tempPerson1.get(i).getfName() + " asked " + tempPerson2.get(i).getfName() +
+                    " out, and " + tempPerson2.get(i).getfName() + " accepted");
+            System.out.println(tempPerson1.get(i).getfName() + " played chess with "  + tempPerson2.get(i).getfName());
+            System.out.println(tempPerson1.get(i).getfName() + " visited " + tempPerson2.get(i).getfName());
+            System.out.println(tempPerson1.get(i).getfName() + " went to dinner with " + tempPerson2.get(i).getfName());
+
+        }
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,26 +1,24 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Person {
     private String fName;
-//    private String gender;
 
     public Person(){
 
     }
 
     public Person(String fName){
+
         this.fName = fName;
-//        this.gender = gender;
     }
 
     public String getfName() {
+
         return fName;
     }
 
     public void setfName(String fName) {
+
         this.fName = fName;
     }
-
 }
